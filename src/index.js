2f213807b9c8e4ebfcf0cbf7380c3611df3ab9a6
Iryna Kolomoiec <kolomoyka@gmail.com>
2022-11-19 23:34:45 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
 import './css/styles.css';
+import { fetchСountries } from './js/fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+
+
+const input = document.querySelector('#search-box')
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
 const DEBOUNCE_DELAY = 300;
+
+
+input.addEventListener('input', 
+debounce(e => {
+    const trimmedValue = input.value.trim(); //ввод інпуту
+    console.log(trimmedValue)
+    clearHtml()
+
+fetchСountries(trimmedValue).then(data => renderOneCountry(data))
+  
+}, DEBOUNCE_DELAY)
+);
+
+
+    
+
+    
+  
+
+//fetchСountries(searchQuery)
+//.then(renderCountryCard)
+//.catch(onFetchError)
+//.finally(() => form.reset());
+
+
+
+
+
+
+
+
+
+
+function renderOneCountry(countries) {
+    const markup = countries
+      .map(country => {
+        return `<li>
+    <img src="${country.flags.svg}"
+    alt="Flag of ${country.name}"
+    width="30" hight="20">
+       <h1>${country.name}</h1>
+          <p><b>Capital</b>: ${country.capital}</p>
+          <p><b>Population</b>: ${country.population}</p>
+          <p><b>Languages</b>: ${Object.values(country.languages)} </p>
+              </li>`;
+      }).join('');
+      
+    countryList.innerHTML = markup;
+}
+
+
+   
+    function clearHtml() {
+        countryList.innerHTML = '';
+        countryInfo.innerHTML = '';
+      }
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,16 @@
+//const BASE_URL = `https://restcountries.com/v2/name/`
+
+export function fetchСountries (name) {
+    //console.log(name);
+    
+    return fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`)
+    .then(resp => {
+        console.log(resp)
+        if(!resp.ok){
+  throw new Error(resp.statusText); //Страхуємо себе від того, що 404 помилка для браузера не є помилка
+        }
+        return resp.json();
+    })//.then(data => console.log(data)) // data повернулась з resp.json
+    .catch(err => error(err))
+    
+}
