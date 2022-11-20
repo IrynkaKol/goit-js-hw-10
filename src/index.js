4ccf9a65ae076039db08fa5da19dbb2ad26ad864
Iryna Kolomoiec <kolomoyka@gmail.com>
2022-11-20 09:29:54 +0100

    uodate

--- src/index.js
+++ src/index.js
@@ -15,29 +15,42 @@ input.addEventListener('input',
 debounce(e => {
     const trimmedValue = input.value.trim(); //ввод інпуту
     console.log(trimmedValue)
-    clearHtml()
+    clearHtml();
 
-fetchСountries(trimmedValue).then(data => renderOneCountry(data))
-  
+    if (trimmedValue !== '') {
+fetchСountries(trimmedValue).then(data => {
+    console.log(data)
+    
+    if(data.lenght > 10) {
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+    } else if (data.lenght === 0) {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+
+    } else if (data.lenght >= 2 && data.lenght <= 10) {
+        renderCountryList(data);
+
+    } else if (data.lenght === 1) {
+    renderOneCountry(data);
+    } 
+});
+    }
 }, DEBOUNCE_DELAY)
 );
 
 
-    
-
-    
-  
-
-//fetchСountries(searchQuery)
-//.then(renderCountryCard)
-//.catch(onFetchError)
-//.finally(() => form.reset());
-
-
-
-
-
-
+function renderCountryList(countries) {
+    const markup = countries
+      .map(country => {
+        return `<li>
+        <img src="${country.flags.svg}" alt="Flag of ${
+          country.name
+        }" width="30" hight="20">
+           <b>${country.name}</p>
+                  </li>`;
+      })
+      .join('');
+    countryList.innerHTML = markup;
+  }
 
 
 
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,12 +1,16 @@
-//const BASE_URL = `https://restcountries.com/v2/name/`
+//const BASE_URL = `https://restcountries.com/v3.1/name/`
 
 export function fetchСountries (name) {
     //console.log(name);
     
     return fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`)
     .then(resp => {
-        console.log(resp)
+        //console.log(resp)
         if(!resp.ok){
+            if (response.status === 404) {
+                return [];
+              }
+            
   throw new Error(resp.statusText); //Страхуємо себе від того, що 404 помилка для браузера не є помилка
         }
         return resp.json();
